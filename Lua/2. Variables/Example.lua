3b2518c4d0098aa5a02a97d581000c8b6bd3eeff
rgjx <161965410+rgjx@users.noreply.github.com>
2024-03-14 21:54:00 -0400

    Update Example.lua

--- Lua/2. Variables/Example.lua
+++ Lua/2. Variables/Example.lua
@@ -1,1 +1,14 @@
+--- SIMPLE VARIABLE:
 
+local sex = "Male"
+local name = "Dill"
+local age = 14
+local discord = rgjx
+
+print("Gender:", sex)
+print("Name:", name)
+print("Age:", age)
+print("Discord:", discord)
+
+
+--- ADVANCED WILL NOT BE PROVIDED FOR THIS
