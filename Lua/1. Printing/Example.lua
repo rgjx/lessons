efb8f67e93a388e6892f99bbb72e44092b8b1d73
rgjx <161965410+rgjx@users.noreply.github.com>
2024-03-14 21:48:00 -0400

    example for #1 printing

--- Lua/1. Printing/Example.lua
+++ Lua/1. Printing/Example.lua
@@ -1,1 +1,24 @@
+SIMPLE:
+a = lua
+print(a) -- this would print "a" into console. (F9) or /console to check it
+ADVANCED:
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+
+local function check()
+    if humanoid.Health <= 0 then
+        print("dead")
+    else
+        print("alive")
+    end
+end
+
+
+while true do
+    check()
+    wait(5) 
+end
+-- this whole thing would print every 5 seconds the state of the player (Humanoid) if the characters health is = 0 then it would print dead, else alive. 
+-- NOTE THIS MAY NOT WORK FOR YOU I WAS TIRED AND THERE MIGHT BE ERRORS.
 
